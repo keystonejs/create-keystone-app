f96ee77ffe6319c1c545bca8b154cc77b6811f43
mitchellhamilton <mitchell@hamil.town>
2021-03-01 15:10:20 +1000

    Add tests for prod

--- index.test.ts
+++ index.test.ts
@@ -8,6 +8,9 @@ import _treeKill from 'tree-kill';
 
 const treeKill = promisify(_treeKill);
 
+process.env.SESSION_SECRET =
+  'adkjnfanskjdfnkjdsafnswkfjlswndjkfnwajskgfn wedjfbeqkhjfbqewhqewgkqegkjnknjjknwsdfkjnsdfkjn';
+
 // this'll take a while
 jest.setTimeout(100000);
 
@@ -23,15 +26,9 @@ const promiseSignal = (): Promise<void> & { resolve: () => void } => {
 // this will open the browser in a headed mode and open the inspector
 // https://playwright.dev/docs/inspector
 
-let cleanupKeystoneProcess = () => {};
-
-afterAll(() => {
-  cleanupKeystoneProcess();
-});
-
 const promisifiedExecFile = promisify(childProcess.execFile);
 
-if (process.env.CREATE_PROJECT === 'true') {
+if (process.env.CREATE_PROJECT === 'create project') {
   test('can create a basic project', async () => {
     const cwd = tempy.directory();
     let createKeystoneAppProcess = promisifiedExecFile(
@@ -56,90 +53,142 @@ if (process.env.CREATE_PROJECT === 'true') {
 
 let projectDir = path.join(__dirname, 'create-keystone-next-app', 'starter');
 
-// starting keystone is the slowest part of this so we start keystone out of the loop
-test('start keystone', async () => {
-  let keystoneProcess = childProcess.execFile('yarn', ['dev'], {
-    cwd: projectDir,
+const logChunk = (chunk: any) => {
+  console.log(chunk.toString('utf8'));
+};
+
+describe.each(['prod', 'dev'] as const)('%s', (mode) => {
+  let cleanupKeystoneProcess = () => {};
+
+  afterAll(async () => {
+    await cleanupKeystoneProcess();
   });
-  let adminUIReady = promiseSignal();
-  let listener = (chunk: any) => {
-    let stringified = chunk.toString('utf8');
-    console.log(stringified);
-    if (stringified.includes('Admin UI and graphQL API ready')) {
-      adminUIReady.resolve();
-    }
-  };
-  keystoneProcess.stdout!.on('data', listener);
 
-  cleanupKeystoneProcess = () => {
-    keystoneProcess.stdout!.off('data', listener);
-    // childProcess.kill will only kill the direct child process
-    // not necessary the children of the child process
-    treeKill(keystoneProcess.pid);
-  };
+  if (mode === 'dev') {
+    test('start keystone in dev', async () => {
+      let keystoneProcess = childProcess.execFile('yarn', ['dev'], {
+        cwd: projectDir,
+        env: process.env,
+      });
+      let adminUIReady = promiseSignal();
+      let listener = (chunk: any) => {
+        let stringified = chunk.toString('utf8');
+        console.log(stringified);
+        if (stringified.includes('Admin UI and graphQL API ready')) {
+          adminUIReady.resolve();
+        }
+      };
+      keystoneProcess.stdout!.on('data', listener);
 
-  await adminUIReady;
-});
+      cleanupKeystoneProcess = async () => {
+        keystoneProcess.stdout!.off('data', listener);
+        // childProcess.kill will only kill the direct child process
+        // so we use tree-kill to kill the process and it's children
+        await treeKill(keystoneProcess.pid);
+      };
 
-describe.each(['chromium', 'webkit', 'firefox'] as const)(
-  '%s',
-  (browserName) => {
-    let page: playwright.Page = undefined as any;
-    let browser: playwright.Browser = undefined as any;
-    beforeAll(async () => {
-      await deleteAllData();
-      browser = await playwright[browserName].launch();
-      page = await browser.newPage();
-      await page.goto('http://localhost:3000');
-    });
-    test('init user', async () => {
-      await page.fill('label:has-text("Name") >> .. >> input', 'Admin');
-      await page.fill(
-        'label:has-text("Email") >> .. >> input',
-        'admin@keystonejs.com'
-      );
-      await page.click('button:has-text("Set Password")');
-      await page.fill('[placeholder="New Password"]', 'password');
-      await page.fill('[placeholder="Confirm Password"]', 'password');
-      await page.click('button:has-text("Get started")');
-      await page.uncheck('input[type="checkbox"]', { force: true });
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('text=Continue'),
-      ]);
-    });
-
-    test('change name of admin', async () => {
-      await page.click('h3:has-text("Users")');
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('a:has-text("Admin")'),
-      ]);
-      await page.type('label:has-text("Name") >> .. >> input', '1');
-      await page.click('button:has-text("Save changes")');
-      await page.goto('http://localhost:3000/users');
-      expect(await page.textContent('a:has-text("Admin1")')).toBe('Admin1');
-    });
-
-    test('create post', async () => {
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('nav >> text=Posts'),
-      ]);
-      await page.click('button:has-text("Create Post")');
-      await page.fill('input[type="text"]', 'content');
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('form[role="dialog"] button:has-text("Create Post")'),
-      ]);
-      await page.type('input[type="text"]', '1');
-      await page.click('button:has-text("Save changes")');
-    });
-    afterAll(async () => {
-      await browser.close();
+      await adminUIReady;
     });
   }
-);
+
+  if (mode === 'prod') {
+    test('build keystone', async () => {
+      let keystoneBuildProcess = promisifiedExecFile('yarn', ['build'], {
+        cwd: projectDir,
+        env: process.env,
+      });
+      keystoneBuildProcess.child.stdout!.on('data', logChunk);
+      keystoneBuildProcess.child.stderr!.on('data', logChunk);
+      await keystoneBuildProcess;
+    });
+    test('start keystone in prod', async () => {
+      let keystoneProcess = childProcess.execFile('yarn', ['start'], {
+        cwd: projectDir,
+        env: process.env,
+      });
+      let adminUIReady = promiseSignal();
+      let listener = (chunk: any) => {
+        let stringified = chunk.toString('utf8');
+        console.log(stringified);
+        if (stringified.includes('Admin UI and graphQL API ready')) {
+          adminUIReady.resolve();
+        }
+      };
+      keystoneProcess.stdout!.on('data', listener);
+      keystoneProcess.stderr!.on('data', listener);
+
+      cleanupKeystoneProcess = async () => {
+        keystoneProcess.stdout!.off('data', listener);
+        keystoneProcess.stderr!.off('data', listener);
+        // childProcess.kill will only kill the direct child process
+        // so we use tree-kill to kill the process and it's children
+        await treeKill(keystoneProcess.pid);
+      };
+
+      await adminUIReady;
+    });
+  }
+
+  describe.each(['chromium', 'webkit', 'firefox'] as const)(
+    '%s',
+    (browserName) => {
+      let page: playwright.Page = undefined as any;
+      let browser: playwright.Browser = undefined as any;
+      beforeAll(async () => {
+        await deleteAllData();
+        browser = await playwright[browserName].launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000');
+      });
+      test('init user', async () => {
+        await page.fill('label:has-text("Name") >> .. >> input', 'Admin');
+        await page.fill(
+          'label:has-text("Email") >> .. >> input',
+          'admin@keystonejs.com'
+        );
+        await page.click('button:has-text("Set Password")');
+        await page.fill('[placeholder="New Password"]', 'password');
+        await page.fill('[placeholder="Confirm Password"]', 'password');
+        await page.click('button:has-text("Get started")');
+        await page.uncheck('input[type="checkbox"]', { force: true });
+        await Promise.all([
+          page.waitForNavigation(),
+          page.click('text=Continue'),
+        ]);
+      });
+
+      test('change name of admin', async () => {
+        await page.click('h3:has-text("Users")');
+        await Promise.all([
+          page.waitForNavigation(),
+          page.click('a:has-text("Admin")'),
+        ]);
+        await page.type('label:has-text("Name") >> .. >> input', '1');
+        await page.click('button:has-text("Save changes")');
+        await page.goto('http://localhost:3000/users');
+        expect(await page.textContent('a:has-text("Admin1")')).toBe('Admin1');
+      });
+
+      test('create post', async () => {
+        await Promise.all([
+          page.waitForNavigation(),
+          page.click('nav >> text=Posts'),
+        ]);
+        await page.click('button:has-text("Create Post")');
+        await page.fill('input[type="text"]', 'content');
+        await Promise.all([
+          page.waitForNavigation(),
+          page.click('form[role="dialog"] button:has-text("Create Post")'),
+        ]);
+        await page.type('input[type="text"]', '1');
+        await page.click('button:has-text("Save changes")');
+      });
+      afterAll(async () => {
+        await browser.close();
+      });
+    }
+  );
+});
 
 async function deleteAllData() {
   const { PrismaClient } = require(path.join(
