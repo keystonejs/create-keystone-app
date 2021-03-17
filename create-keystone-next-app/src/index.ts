0fc414c17aefc8e5b0bc37a53be0a9d151dd9e52
Jess Telford <jess+github@jes.st>
2021-03-17 13:55:31 +1100

    Use Ora for nicer loading state during dep installation

--- create-keystone-next-app/src/index.ts
+++ create-keystone-next-app/src/index.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import meow from 'meow';
 import enquirer from 'enquirer';
 import execa, { ExecaError } from 'execa';
+import ora from 'ora';
 import { checkVersion } from './checkVersion';
 import { UserError } from './utils';
 import c from 'chalk';
@@ -71,19 +72,23 @@ async function normalizeArgs(): Promise<Args> {
 }
 
 const installDeps = async (cwd: string): Promise<'yarn' | 'npm'> => {
-  console.log(
-    'Installing dependencies with yarn. This will take a few minutes.'
-  );
+  const spinner = ora('Installing dependencies with yarn. This may take a few minutes.').start();
   try {
-    await execa('yarn', ['install'], { cwd, stdio: 'inherit' });
+    await execa('yarn', ['install'], { cwd });
+    spinner.succeed('Installed dependencies with yarn.');
     return 'yarn';
   } catch (_err) {
     let err: ExecaError = _err;
     if (err.failed) {
-      console.log(
-        'Failed to install with yarn. Installing dependencies with npm.'
-      );
-      await execa('npm', ['install'], { cwd, stdio: 'inherit' });
+      spinner.warn('Failed to install with yarn.');
+      spinner.start(Installing dependencies with npm. This may take a few minutes.');
+      try {
+        await execa('npm', ['install'], { cwd });
+        spinner.succeed('Installed dependencies with npm.');
+      } catch (npmErr) {
+        spinner.fail('Failed to install with npm.');
+        throw npmErr;
+      }
       return 'npm';
     }
     throw err;
