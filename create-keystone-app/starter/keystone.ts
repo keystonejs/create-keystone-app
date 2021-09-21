b82ee3bbf13a1bf44a04a0d81eddce06259adb51
Noviny <noviny@thinkmill.com.au>
2021-09-21 10:16:32 +1000

    code comments

--- create-keystone-app/starter/keystone.ts
+++ create-keystone-app/starter/keystone.ts
@@ -1,25 +1,32 @@
+/*
+Welcome to keystone! This file is what keystone uses to start the app.
+
+It looks at the default export, and expects a keystone config object.
+
+You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
+*/
+
 import { config } from '@keystone-next/keystone';
-import { statelessSessions } from '@keystone-next/keystone/session';
 
+// Look in the schema file for how we define our lists, and how users interact with them through graphql or the admin UI
 import { lists } from './schema';
-import { withAuth, sessionSecret } from './auth';
-
-let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-
-const session = statelessSessions({
-  maxAge: sessionMaxAge,
-  secret: sessionSecret!,
-});
+// Keystone auth is easy to set up - check out the basic auth setup in the auth file.
+import { withAuth, session } from './auth';
 
 export default withAuth(
+  // Using the config function ensures our exported config is correct and throws useful errors if we've made a mistake
   config({
+    // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
       provider: 'sqlite',
       url: 'file:./keystone.db',
     },
+    // This config allows us to set up features of the admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
+      // For our starter, we check that someone has session data before letting them see the admin UI.
       isAccessAllowed: (context) => !!context.session?.data,
     },
+    // lists and session config can be a bit big, so we've put them in other files
     lists,
     session,
   })
--- create-keystone-app/starter/schema.ts
+++ create-keystone-app/starter/schema.ts
@@ -1,4 +1,25 @@
+/*
+Welcome to the schema! The schema is the lifeblood of keystone.
+
+Here we define our 'lists', which will then be used both for the graphql
+API definition, our database tables, and our admin UI layout.
+
+Some quick definitions to help out:
+A list: A definition of a collection of fields with a name. For the starter
+  we have `User`, `Post`, and `Tag` lists.
+A field: The individual bits of data on your list, each with its own type.
+  you can see some of the lists in what we use below.
+
+## For this app
+We have 
+*/
+
+// Like the `config` function we use in keystone.ts, we use functions
+// for putting in our config so we get useful errors. With typescript,
+// we get these even before code runs.
 import { createSchema, list } from '@keystone-next/keystone';
+// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
+// for the full list of fields.
 import {
   text,
   relationship,
@@ -6,15 +27,24 @@ import {
   timestamp,
   select,
 } from '@keystone-next/keystone/fields';
+// The document field is a more complicated field, so it's in its own package
+// Keystone aims to have all the base field types, but you can make your own
+// custom ones.
 import { document } from '@keystone-next/fields-document';
 
+// Each property on the object passed in to `createSchema` will become a
+// list in keystone.
 export const lists = createSchema({
+  // Here we define the user list.
   User: list({
+    // Here we can modify the admin UI. We want to show a user's name and post in the admin UI
     ui: {
       listView: {
         initialColumns: ['name', 'posts'],
       },
     },
+    // Here are the fields that `User` will have. We want an email and password so they can log in
+    // a name so we can refer to them, and a way to connect users to posts.
     fields: {
       name: text({ isRequired: true }),
       email: text({
@@ -22,18 +52,30 @@ export const lists = createSchema({
         isIndexed: 'unique',
         isFilterable: true,
       }),
+      // Passwords are hard. Our password field takes care of password problems
       password: password({ isRequired: true }),
+      // relationships allow us to reference other lists. In this case,
+      // we want a user to have many posts, and we are saying that the user
+      // should be referencable by the 'author' field of posts.
+      // Relationships are complicated. Make sure you read the docs
+      // to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
       posts: relationship({ ref: 'Post.author', many: true }),
     },
   }),
+  // Our second list is the Posts list. We've got a few more fields here
+  // so we have all the info we need for displaying posts.
   Post: list({
     fields: {
       title: text(),
+      // Having the status here will make us easy to choose whether to display
+      // posts on a live site.
       status: select({
         options: [
           { label: 'Published', value: 'published' },
           { label: 'Draft', value: 'draft' },
         ],
+        // We want to make sure new posts start off as a draft when they are created
+        defaultValue: 'draft',
         ui: {
           displayMode: 'segmented-control',
         },
@@ -51,6 +93,8 @@ export const lists = createSchema({
         dividers: true,
       }),
       publishDate: timestamp(),
+      // Here is the link from post => author.
+      // We've customised its UI display quite a lot.
       author: relationship({
         ref: 'User.posts',
         ui: {
@@ -61,6 +105,7 @@ export const lists = createSchema({
           inlineCreate: { fields: ['name', 'email'] },
         },
       }),
+      // We also link posts to tags. This is a many <=> many linking.
       tags: relationship({
         ref: 'Tag.posts',
         ui: {
@@ -75,6 +120,7 @@ export const lists = createSchema({
       }),
     },
   }),
+  // Our final list is the tag list. This field is just a name and a relationship to posts
   Tag: list({
     ui: {
       isHidden: true,
